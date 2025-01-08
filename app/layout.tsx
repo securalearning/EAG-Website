51c185f6dd02fb2a60c15883fd6dc6472215e9f2
SecuraLearning <coachdarshana@gmail.com>
2025-01-08 19:47:07 +0530

    updated the favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,17 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <title>EA Global - Your Gateway to International Education</title>
-        <meta name="description" content="EA Global is Chennai's premier educational consultancy specializing in study abroad services and English proficiency test preparation." />
+        <meta
+          name="description"
+          content="EA Global is Chennai's best educational consultancy specializing in study abroad services and English proficiency test preparation."
+        />
+        {/* Favicon setup */}
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='%230008ff' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><path d='M21.42 10.922a1 1 0 0 0-.019-1.838L12.83 5.18a2 2 0 0 0-1.66 0L2.6 9.08a1 1 0 0 0 0 1.832l8.57 3.908a2 2 0 0 0 1.66 0z'/><path d='M22 10v6'/><path d='M6 12.5V16a6 3 0 0 0 12 0v-3.5'/></svg>"
+        />
+        <link rel="alternate icon" href="/favicon.ico" />
       </head>
       <body className={`${inter.className} min-h-screen bg-background`}>
         <ThemeProvider
@@ -30,9 +40,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <Navigation />
-          <main className="flex-1">
-            {children}
-          </main>
+          <main className="flex-1">{children}</main>
           <Footer />
           <Toaster />
         </ThemeProvider>
