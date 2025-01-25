e76ed0c5df7d314c9d195716d978acafac071314
SecuraLearning <coachdarshana@gmail.com>
2025-01-26 01:09:23 +0530

    Update metadata URLs and descriptions for EA Global. Also tried updating Opengraph image

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,12 +14,12 @@ import UrlHandler from '@/components/UrlHandler';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  metadataBase: new URL('https://www.englisharenaglobal.com'),
+  metadataBase: new URL('https://studyabroad.englisharenaglobal.com'),
   title: {
     default: 'EA Global - Your Gateway to International Education',
     template: '%s | EA Global - Your Gateway to International Education',
   },
-  description: 'EA Global is Chennai\'s best educational consultancy specializing in study abroad services and English proficiency test preparation.',
+  description: 'EA Global (formerly English Arena Global) is Chennai\'s best educational consultancy specializing in study abroad services and English proficiency test preparation. Get expert guidance and personalized coaching to achieve your academic and career goals.',
   icons: {
     icon: [
       {
@@ -32,15 +32,14 @@ export const metadata = {
   },
   openGraph: {
     type: 'website',
-    url: 'https://studyoverseas.netlify.app',
+    url: 'https://studyabroad.englisharenaglobal.com',
     title: 'EA Global - Your Gateway to International Education',
     description: 'EA Global (formerly English Arena Global) is Chennai\'s best educational consultancy specializing in study abroad services and English proficiency test preparation, including IELTS, PTE, CELPIP, and OET. Get personalized coaching, mock tests, and expert guidance to achieve your academic and career goals.',
     images: [
       {
-        //url: '/images/og-image.png',
-        url: 'https://opengraph.b-cdn.net/production/images/0f8d4b4c-da0f-4e28-84bc-c642dc2e41b6.png?token=OQ87ZR3zcb1YGR2iPejzaCgoovTxRM2CfwEKolnz7mk&height=632&width=1200&expires=33273557911',
+        url: '/app/opengraph-image.png',
         width: 1200,
-        height: 632,
+        height: 630,
         alt: 'EA Global Logo',
       },
     ],
@@ -48,11 +47,11 @@ export const metadata = {
   twitter: {
     card: 'summary_large_image',
     domain: 'englisharenaglobal.com',
-    url: 'https://www.englisharenaglobal.com/',
+    url: 'https://studyabroad.englisharenaglobal.com',
     title: 'EA Global - Your Gateway to International Education',
     description: 'EA Global is Chennai\'s best educational consultancy specializing in study abroad services and English proficiency test preparation.',
-    //image: '/images/og-image.png',
-    image: 'https://opengraph.b-cdn.net/production/images/0f8d4b4c-da0f-4e28-84bc-c642dc2e41b6.png?token=OQ87ZR3zcb1YGR2iPejzaCgoovTxRM2CfwEKolnz7mk&height=632&width=1200&expires=33273557911',
+    //image: 'https://opengraph.b-cdn.net/production/images/0f8d4b4c-da0f-4e28-84bc-c642dc2e41b6.png?token=OQ87ZR3zcb1YGR2iPejzaCgoovTxRM2CfwEKolnz7mk&height=632&width=1200&expires=33273557911',
+    image: '/app/opengraph-image.png',
   },
 };
 
